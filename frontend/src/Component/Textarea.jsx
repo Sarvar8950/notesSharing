8855c82c29f2d56b038ba1dc7b0efd83b8291e8e
Sarvar Gulia <sarvar8950@gmail.com>
2024-01-08 12:58:24 +0530

    Delete frontend directory

--- frontend/src/Component/Textarea.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Textarea({setText}) {
-  return (
-    <div className='w-3/4 m-auto my-4 h-3/4'>
-        <textarea className='w-full h-full p-4' placeholder='Enter Your Text' onChange={(e) => setText(e.target.value)}></textarea>
-    </div>
-  )
-}
