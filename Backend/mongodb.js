02f6bf772bd7c08062704d3b49532572a5b501fb
Sarvar Gulia <sarvar8950@gmail.com>
2024-01-08 12:58:08 +0530

    Delete Backend directory

--- Backend/mongodb.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://<userName>:<password>@cluster0.z6yezz3.mongodb.net/?retryWrites=true&w=majority`, () => {
-    console.log("mongodb is running")
-})
-module.exports = mongoose
--- Backend/schema.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const text = new mongoose.Schema({
-    data : String
-})
-
-module.exports = mongoose.model('onlineNotes', text)
